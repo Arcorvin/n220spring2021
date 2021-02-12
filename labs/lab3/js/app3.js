b45a6d36db33b92804e4dfc67f8e5906e6ee44ae
Arcorvin <77901990+Arcorvin@users.noreply.github.com>
2021-02-12 00:37:14 -0500

    pyramid update

--- labs/lab3/js/app3.js
+++ labs/lab3/js/app3.js
@@ -4,10 +4,11 @@ function setup(){
 
 function draw(){
 fill(255,0,0);
-stroke(255);
+stroke(255)
         for( var y = 1; y <= 4; y++){
             for(var x = 1; x <= y; x++){
-              square(x*25,y*25,25);  
+              square(x*75,y*75,75);
+              
             }
 
         }
