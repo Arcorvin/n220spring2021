ffb7818996de0cd7f91c85d41bbee0548e63ded8
Arcorvin <77901990+Arcorvin@users.noreply.github.com>
2021-03-19 19:52:40 -0400

    Midterm Update

--- labs/midterm/js/app.js
+++ labs/midterm/js/app.js
@@ -3,12 +3,12 @@
 
 
 
-
+/* code for making the 10 circles appear and changing of sizes*/
 function myCircle(){
     var dia = 25;
     for(var i = 0; i < 10; i++){
     circle(mouseX, mouseY, dia);
-    scale(dia%.9)
+    scale(dia%.9) /* changes the size of the circles*/
 }
 }
 
@@ -17,13 +17,13 @@ function myCircle(){
 /*this will be for the basic set up of the program i.e. backgrounds and canvas size*/
 function setup(){
     createCanvas(900,900);
-    x = width/2;
 }
 
 function draw(){
     background(0);
-    myCircle();
+    myCircle(); /*allows the fuction of myCircle to be drawn*/
 
+    /*code for chaging the color*/
     for(var i= 0; i<900;i++){
         fill(190)
     }
