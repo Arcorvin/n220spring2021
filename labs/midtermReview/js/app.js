971e042aaa40e56b216327d702121acbf2935149
Arcorvin <77901990+Arcorvin@users.noreply.github.com>
2021-03-18 23:50:14 -0400

    review2

--- labs/midtermReview/js/app.js
+++ labs/midtermReview/js/app.js
@@ -29,13 +29,13 @@ function draw(){
     circle(x,y,myPadd.s)*/
 
 
-    /*myBall.x += myBall.sped*myBall.dir;
-    if((myBall.x>myBall.width-myBall.radius) || (myBall.x < myBall.radius)){
+    myBall.x2 += myBall.sped*myBall.dir;
+    if((myBall.x2>myBall.width-myBall.radius) || (myBall.x < myBall.radius)){
         myBall.dir = - myBall.dir;
     }
     if(myBall.dir == 1){
-        arc(myBall.x,60, myBall.radius, myBall.radius, 0.52, 5.76);
+        arc(myBall.x2,60, myBall.radius, myBall.radius, 0.52, 5.76);
     }else{
-        arc(myBall.x,60,myBall.radius,myBall.radius,3.67, 8.7);
-    }*/
+        arc(myBall.x2,60,myBall.radius,myBall.radius,3.67, 8.7);
+    }
 }
