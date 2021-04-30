3fd3a17702ac1e855275025dab21947265332b72
Arcorvin <77901990+Arcorvin@users.noreply.github.com>
2021-04-29 23:47:57 -0400

    lab10

---
+++ labs/lab10/js/app.js
@@ -0,0 +1,33 @@
+
+let dvBox = document.getElementById("evTest")
+dvBox.addEventListener("click", colCha);
+let dvBox2 = document.getElementById("evTest2")
+dvBox2.addEventListener("click", colCha);
+let dvBox3 = document.getElementById("evTest3")
+dvBox3.addEventListener("click", colCha);
+
+dvBox.style.backgroundColor = "#999";
+dvBox.style.width = "200px";
+dvBox.style.height = "200px";
+dvBox.style.float = "left";
+dvBox.style.margin = "5px";
+
+dvBox2.style.backgroundColor = "#999";
+dvBox2.style.width = "200px";
+dvBox2.style.height = "200px";
+dvBox2.style.float = "left";
+dvBox2.style.margin = "5px";
+
+dvBox3.style.backgroundColor = "#999";
+dvBox3.style.width = "200px";
+dvBox3.style.height = "200px";
+dvBox3.style.float = "left";
+dvBox3.style.margin = "5px";
+
+
+function colCha(event){
+
+    event.target.style.backgroundColor = "#000e0";
+    
+
+}
